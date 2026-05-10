585434e66599756f1cf506f7ced185e6d252e000
sslaia <laia.sirus@gmail.com>
2026-05-10 11:27:38 +0100

    Small fixes

--- lib/utils/shortcut_utils.dart
+++ lib/utils/shortcut_utils.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import '../models/project_type.dart';
 import '../screens/article_screen.dart';
 
 class ShortcutUtils {
